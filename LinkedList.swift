4e13d5334d38fa6878bb6c0e615e035454139bc7
Nitkarsh Gupta <nitkarshg@gmail.com>
2021-06-13 02:32:15 +0530

    Added implementation for linked list

---
+++ LinkedList.swift
@@ -0,0 +1,108 @@
+//
+//  LinkedList.swift
+//
+//  Created by Nitkarsh Gupta on 13/06/21.
+//
+
+import Foundation
+
+import Foundation
+
+final class Node<T> {
+    let value: T
+    weak var previousNode: Node<T>?
+    var nextNode: Node<T>?
+    
+    init(_ value: T) {
+        self.value = value
+    }
+}
+
+final class LinkedList<T: Hashable> {
+    private var head: Node<T>?
+    private var tail: Node<T>?
+    
+    private var _privateDict: [T: Node<T>] = [:]
+    
+    init(_ values: [T] = []) {
+        for item in values {
+            self.insert(item)
+        }
+    }
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    var first: T? {
+        return head?.value
+    }
+    
+    var last: T? {
+        return tail?.value
+    }
+    
+    func insert(_ value: T) {
+        let newNode = Node(value)
+        
+        guard head != nil else {
+            head = newNode
+            tail = newNode
+            _privateDict[value] = newNode
+            return
+        }
+        
+        defer {
+            tail?.nextNode = newNode
+            newNode.previousNode = tail
+            _privateDict[value] = newNode
+            tail = newNode
+        }
+        
+        guard let oldNode = _privateDict[value] else {
+            return
+        }
+        
+        let previousNode = oldNode.previousNode
+        let nextNode = oldNode.nextNode
+        if let previousNode = previousNode {
+            if tail?.value == value {
+                tail = previousNode
+            } else if tail?.previousNode?.value == value {
+                tail?.previousNode = previousNode
+            }
+            previousNode.nextNode = nextNode
+        } else {
+            head = head?.nextNode == nil ? newNode : nextNode
+        }
+    }
+    
+    func peekObject() -> T? {
+        guard let element = head?.value else {
+            return nil
+        }
+        let nextNode = head?.nextNode
+        head = nextNode
+        _privateDict.removeValue(forKey: element)
+        if nextNode == nil {
+            tail = nil
+            head = nil
+        }
+        return element
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    var description: String {
+        var initialWord = "["
+        var node = head
+        while node != nil {
+            initialWord.append("\(node!.value)")
+            if node?.nextNode != nil { initialWord.append(", ") }
+            node = node?.nextNode
+        }
+        initialWord.append("]")
+        return initialWord
+    }
+}
+
